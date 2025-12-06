d1c82005e8164130dfe455d5cc8a72211ec442db
Maciej Krzyżanowski <krzmaciek@gmail.com>
2025-12-06 23:11:18 +0100

    day2: Progress

--- day2/src/main.zig
+++ day2/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const day2 = @import("day2");
+const expect = @import("std").testing.expect;
+const math = @import("std").math;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -13,4 +14,131 @@ pub fn main() !void {
 
     _ = try stdinReader.interface.streamDelimiter(&lineWriter.writer, '\n');
     std.debug.print("{s}\n", .{lineWriter.written()});
+
+    var rangesIter = std.mem.splitScalar(u8, lineWriter.written(), ',');
+    while (rangesIter.next()) |range| {
+        var rangeIter = std.mem.splitScalar(u8, range, '-');
+        const rangeStartStr = rangeIter.next() orelse return error.InputDataIncorrect;
+        const rangeEndStr = rangeIter.next() orelse return error.InputDataIncorrect;
+        const rangeStart = try std.fmt.parseInt(u64, rangeStartStr, 10);
+        const rangeEnd = try std.fmt.parseInt(u64, rangeEndStr, 10);
+
+        std.log.debug("{d} up to {d}", .{ rangeStart, rangeEnd });
+    }
+}
+
+fn generateInvalids(allocator: std.mem.Allocator, rangeStartInclusive: u64, rangeEndInclusive: u64) !void {
+    var list: std.ArrayList(u64) = .empty;
+    defer list.deinit(allocator);
+
+    try list.append(allocator, rangeStartInclusive);
+    try list.append(allocator, rangeEndInclusive);
+}
+
+test "generate invalids" {
+    try generateInvalids(std.testing.allocator, 0, 10);
+}
+
+fn getStartingHalf(allocator: std.mem.Allocator, startFrom: u64) !u64 {
+    const smallestEven = findSmallestDigitEven(startFrom);
+    var startingHalf = try getFirstHalfOfNumber(allocator, smallestEven);
+    var result = concatDupNumber(startingHalf);
+
+    while (result < startFrom) {
+        startingHalf += 1;
+        result = concatDupNumber(startingHalf);
+    }
+
+    return startingHalf;
+}
+
+test "get starting half" {
+    var startingHalf = try getStartingHalf(std.testing.allocator, 1);
+    try expect(startingHalf == 1);
+
+    startingHalf = try getStartingHalf(std.testing.allocator, 1234);
+    try expect(startingHalf == 13);
+}
+
+fn concatDupNumber(half: u64) u64 {
+    const halfDigitsCount = countDigits(half);
+    var result = half;
+
+    result = result + math.pow(u64, 10, halfDigitsCount) * result;
+
+    return result;
+}
+
+test "concat dup number" {
+    try expect(concatDupNumber(123) == 123123);
+}
+
+fn findSmallestDigitEven(startFrom: u64) u64 {
+    const startFromDigitsCount = countDigits(startFrom);
+
+    if (startFromDigitsCount % 2 != 0) {
+        return math.pow(u64, 10, startFromDigitsCount);
+    } else {
+        return startFrom;
+    }
+}
+
+test "find smallest digit even" {
+    try expect(findSmallestDigitEven(1) == 10);
+    try expect(findSmallestDigitEven(774) == 1000);
+}
+
+fn getFirstHalfOfNumber(allocator: std.mem.Allocator, num: u64) !u64 {
+    const numDigitsCount = countDigits(num);
+
+    if (numDigitsCount % 2 != 0) {
+        return error.DigitsCountNotEven;
+    }
+
+    var currNum = num;
+    var currDigitIdx: u64 = 0;
+    var digits = try allocator.alloc(u64, numDigitsCount);
+    defer allocator.free(digits);
+
+    while (currDigitIdx < numDigitsCount) {
+        const digit = currNum % 10;
+
+        digits[currDigitIdx] = digit;
+
+        currNum /= 10;
+        currDigitIdx += 1;
+    }
+
+    var currHalfIdx: u64 = 0;
+    var result: u64 = 0;
+
+    while (currHalfIdx < numDigitsCount / 2) {
+        const digitIdx = numDigitsCount / 2 + currHalfIdx;
+        result += math.pow(u64, 10, currHalfIdx) * digits[digitIdx];
+        currHalfIdx += 1;
+    }
+
+    return result;
+}
+
+test "get first half of number" {
+    const half = try getFirstHalfOfNumber(std.testing.allocator, 112233);
+    try expect(half == 112);
+}
+
+fn countDigits(x: u64) u64 {
+    var digitCount: u32 = 0;
+    var currX = x;
+
+    while (currX != 0) {
+        digitCount += 1;
+        currX /= 10;
+    }
+
+    return digitCount;
+}
+
+test "digit counting" {
+    try expect(countDigits(1) == 1);
+    try expect(countDigits(123) == 3);
 }
